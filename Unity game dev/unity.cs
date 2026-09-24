2c92c5f4cc1a42ed79a6046884c61b475a31d299
EvanWarkentin <e.warkentin40@gmail.com>
2026-09-24 20:06:39 +0000

    Learned more code.

--- Unity game dev/unity.cs
+++ Unity game dev/unity.cs
@@ -10,8 +10,32 @@ Console.WriteLine ("hello");
 Console.WriteLine ("his name is ");
 
 string name = "Evan";
+
 Console.WriteLine (name);
 
-int number = "16";
+Console.WriteLine ("and he is");
 
-Console.WriteLine (number);
+int myNum = 16;
+
+Console.WriteLine (myNum);
+
+myNum = 17;
+
+Console.WriteLine ("Updated age " + myNum);
+
+const int PermNumber = 35; 
+
+Console.WriteLine ("This number will always be the same " + PermNumber);
+
+string Firstname = "John ";
+string Lastname = "Doe";
+string Fullname = Firstname + Lastname;
+
+Console.WriteLine (Fullname);
+
+int x = 10;
+int y = 15;
+Console.WriteLine ("this number should be 10 + 15");
+Console.WriteLine (x + y);
+
+
