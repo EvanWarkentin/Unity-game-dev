6d5f51485a214c9568e4ff335726eba0240197ad
EvanWarkentin <e.warkentin40@gmail.com>
2026-09-24 19:43:27 +0000

    learning more c#

---
+++ Unity game dev/unity.cs
@@ -0,0 +1,17 @@
+//hello this is a Comment
+
+/* this is a multi
+line comment */
+
+Console.WriteLine ("Hello World!"); 
+
+Console.WriteLine ("hello");
+
+Console.WriteLine ("his name is ");
+
+string name = "Evan";
+Console.WriteLine (name);
+
+int number = "16";
+
+Console.WriteLine (number);
